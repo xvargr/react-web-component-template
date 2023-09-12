d409c8705269a50fa88e25f3d70eb0614cc96a2a
xvargr <targenta650@gmail.com>
2023-09-12 10:12:07 +0800

    add dynamic target and meta tag, add global vars

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.scss";
 
+const target = (
+  document.getElementsByName("component-target-id")[0] as HTMLMetaElement
+)?.content;
+
+if (!target) throw new Error("No  component target specified");
+
 // Select your shadow host
-const shadowHost = document.getElementById("react-web-component-target")!;
+const shadowHost = document.getElementById(target)!;
 
 // Create the shadow root
 const shadowRoot = shadowHost.attachShadow({ mode: "open" });
